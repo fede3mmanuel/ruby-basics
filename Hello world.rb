5580753b78c14babefa852812ec2251b06ac2aa9
Emmanuel Sanchez (Fede) <fede@fede.website>
2020-10-28 17:03:40 -0600

    if2 statements on ruby

--- Hello world.rb
+++ Hello world.rb
@@ -1,12 +1,13 @@
-ismale = false
-istall = true
 
-if ismale and istall
-    puts "You are a tall male"
-elsif ismale and !istall
-    puts "You are a short male"
-elsif !ismale and istall
-    puts "You are not male but are tall"
-else
-    puts "You are not male and not tall"
+
+def max(num1, num2, num3)
+    if num1 >= num2 and num1 >= num3
+        return num1
+    elsif num2 >= num1 and num2 >= num3
+        return num2
+    else
+        return num3
+    end
 end
+
+puts max(100,20,3)

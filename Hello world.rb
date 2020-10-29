d5c680d96b789888636a7e21a1f7cd2f12fc107d
Emmanuel Sanchez (Fede) <fede@fede.website>
2020-10-29 12:21:32 -0600

    For loops2 on ruby

--- Hello world.rb
+++ Hello world.rb
@@ -1,18 +1,11 @@
 
-friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+def pow(base_num, pow_num)
+    result = 1
 
-for friend in friends
-    puts friend
+    pow_num.times do
+        result = result * base_num
+    end
+    return result
 end
 
-friends.each do |friend|
-    puts friend
-end
-
-for index in 0..5
-    puts index
-end
-
-6.times do |index|
-    puts index
-end
+puts pow(2, 3)

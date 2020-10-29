e7cce3882a6895394cb58297262485aac127ac0e
Emmanuel Sanchez (Fede) <fede@fede.website>
2020-10-29 10:58:59 -0600

    Guessing game on ruby

--- Hello world.rb
+++ Hello world.rb
@@ -1,8 +1,24 @@
 
 
-index = 1
+secret_word = "dog"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
 
-while index <= 5
-    puts index
-    index += 1
+while guess != secret_word and !out_of_guesses
+    if guess_count < guess_limit
+        puts "Enter guess: "
+        guess = gets.chomp()
+        guess_count += 1
+    else
+        out_of_guesses = true
+    end
+    
+end
+
+if out_of_guesses
+    puts "You lose"
+else
+    puts "You Won!"
 end

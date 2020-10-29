7b710f392dca0c404ec19222ce41b6f9bda673f5
Emmanuel Sanchez (Fede) <fede@fede.website>
2020-10-29 15:14:38 -0600

    Write files on ruby

--- Hello world.rb
+++ Hello world.rb
@@ -1,14 +1,8 @@
 
-File.open("employees.txt", "r") do |file|
+File.open("employees.txt", "r+") do |file|
 
-    for line in file.readlines()
-        puts line
-    end
+    file.readline()
+    file.write("Overriden")
 
 end
 
-file = File.open("employees.txt", "r")
-
-puts file.read
-
-file.close()

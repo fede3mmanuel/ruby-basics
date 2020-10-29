29bde1ab070a9ddf6f6b042170479858e512a0c9
Emmanuel Sanchez (Fede) <fede@fede.website>
2020-10-29 16:55:11 -0600

    Object methods on ruby

--- Hello world.rb
+++ Hello world.rb
@@ -1,17 +1,20 @@
-class Book
-    attr_accessor :title, :author, :pages
+class Student
+    attr_accessor :name, :major, :gpa
+    def initialize (name, major, gpa)
+        @name = name
+        @major = major
+        @gpa = gpa
+    end
 
-    def initialize(title, author, pages)
-        @title = title
-        @author = author
-        @pages = pages
+    def has_honors
+        if @gpa >= 3.5
+            return true
+        end
+        return false
     end
 end
 
-book1 = Book.new("Harry Potter", "JK Rowling", 400)
+student1 = Student.new("Jim", "Business", 2.6)
+student2 = Student.new("Pam", "Art", 3.6)
 
-
-book2 = Book.new("Lord of the rings", "Tolkein", 500)
-
-
-puts book2.title
+puts student2.has_honors
